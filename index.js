097b9e8483e5c27c279f10d68657d63a4d191530
Lucas <lukkotecki@gmail.com>
2023-10-23 00:05:19 +0200

    skonczenie pierwszego zadania dodatkowego

--- index.js
+++ index.js
@@ -11,14 +11,23 @@ const toInput = document.getElementById("to-input");
 import { databaseAddress } from "/database.js" 
 const app = initializeApp(databaseAddress);
 const database = getDatabase(app);
-const endorsementsInDatabase = ref(database, "endorsements");
+const endorsementsInDatabase = ref(database, "endorsements2");
 
-let endorsements = ["Super ci poszło", "to jest świetne", "wszyscy wygrali"];
+let endorsementObject = {
+    endorsement: "",
+    from: "",
+    to: "",
+    likes: 0
+}
 
 publishBtn.addEventListener("click", function(){
-    endorsements.push(endorsementInput.value)
-    push(endorsementsInDatabase, endorsementInput.value)
-    //renderEndorsements(endorsements);
+    //push(endorsementsInDatabase, endorsementInput.value)
+    endorsementObject.endorsement = endorsementInput.value;
+    endorsementObject.from = fromInput.value;
+    endorsementObject.to = toInput.value;
+    endorsementObject.likes = 0;
+
+    push(endorsementsInDatabase, endorsementObject)
     clearEndorsemenInput();
 });
 
@@ -27,11 +36,22 @@ onValue(endorsementsInDatabase, function(snapshot){
     //Object.entries() - zamienia obiekt na array
 
     clearEndorsementsArea();
+    
     if (snapshot.exists()){
-        let endorsementsFromDatabase = Object.entries(snapshot.val()) 
-        for(let i=0; i<endorsementsFromDatabase.length; i++){
-            addEndorsementElement(endorsementsFromDatabase[i]);
-        }
+        // let endorsementsFromDatabase = Object.entries(snapshot.val()) 
+        // for(let i=0; i<endorsementsFromDatabase.length; i++){
+        //     addEndorsementElement(endorsementsFromDatabase[i]);
+        // }
+        let endorsementsFromDatabase = Object.entries(snapshot.val());
+
+        endorsementsFromDatabase.forEach((element) => addEndorsementElement(element))
+        // console.log(endorsementsFromDatabase)
+        // for (let i=0; i<endorsementsFromDatabase.length; i++){
+        //     console.log(endorsementsFromDatabase[i][0])
+        //     console.log(endorsementsFromDatabase[i][1].likes)
+        // }
+
+
     }else{
         endorsementsArea.textContent = "Nothing to show yet..."
     }
@@ -46,17 +66,43 @@ function clearEndorsementsArea(){
     endorsementsArea.innerHTML = ""
 }
 
-function addEndorsementElement(endorsemenToAdd){
+function addEndorsementElement(endorsementToAdd){
+    // let elementToAppend = document.createElement("li");
+    // let keyOfEndorsement = endorsementToAdd[0];
+    // let valueOfEndorsement = endorsementToAdd[1];
+    // elementToAppend.textContent = valueOfEndorsement;
+    // elementToAppend.addEventListener("click",function(){
+    //     remove(ref(database, "endorsements/"+keyOfEndorsement))
+    // })
+    // endorsementsArea.append(elementToAppend)
+    console.log("");
+    console.log("KOLEJNY ELEMENT")
     let elementToAppend = document.createElement("li");
-    let keyOfEndorsement = endorsemenToAdd[0];
-    let valueOfEndorsement = endorsemenToAdd[1];
-    elementToAppend.textContent = valueOfEndorsement;
-    
-    elementToAppend.addEventListener("click",function(){
-        remove(ref(database, "endorsements/"+keyOfEndorsement))
-    })
+    let keyOfEndorsement = endorsementToAdd[0];
+    let valueOfEndorsement = endorsementToAdd[1].endorsement;
+    let fromEndorsement = endorsementToAdd[1].from;
+    let toEndorsement = endorsementToAdd[1].to;
+    let likesEndorsement = endorsementToAdd[1].likes;
 
-    endorsementsArea.append(elementToAppend)
+    console.log("KEY: "+keyOfEndorsement);
+    console.log("VALUE: "+valueOfEndorsement);
+    console.log("FROM: "+fromEndorsement);
+    console.log("TO: "+toEndorsement)
+    console.log("LIKES: "+likesEndorsement);
+
+
+    elementToAppend.innerHTML = 
+    `<h4>To ${toEndorsement}</h3>
+    <div class="value-of-endorsement">${valueOfEndorsement}</div> 
+    <div class="last-line">
+        <h5>From ${fromEndorsement}</h5> 
+        <div class="likes"> ❤️ ${likesEndorsement}</div>
+    </div>`
+
+    elementToAppend.addEventListener("click",function(){
+        remove(ref(database, "endorsements2/"+keyOfEndorsement));
+    })
+    endorsementsArea.append(elementToAppend);
 
 
 }
